6aca17c6224108bf042384a82b4fb6fb4e42dfae
mtorgovitsky <m.torgovitsky@gmail.com>
2016-11-11 10:16:22 +0200

    Add project files.

---
+++ RecursionArraySortedOrNot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionArraySortedOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] iArray1 = { 3, 2, 6, 70, 1, 65, 698, 2, 31, 45 };
+            int[] iArray2 = { 1, 2, 6, 70, 76, 963, 7952, 12366, 23548};
+            Console.WriteLine("Array is sorted = {0}", SortedOrNot(iArray1, iArray1.Length - 1));
+            Console.WriteLine("Array is sorted = {0}", SortedOrNot(iArray2, iArray2.Length - 1));
+        }
+
+        #region If Array is Array Sorted
+        //Checking if an array is sorted recursively
+        static bool SortedOrNot(int[] arr, int iMaxIndex)
+        {
+            if (iMaxIndex == 0)
+            {
+                return true;
+            }
+
+            return arr[iMaxIndex - 1] <= arr[iMaxIndex] && SortedOrNot(arr, iMaxIndex - 1);
+        }
+
+        #endregion
+    }
+}
